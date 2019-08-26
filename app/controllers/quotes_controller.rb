aa68d5d290a693d0fccba251e0813356d34a6f5c
lien nguyen <trucng@yahoo.com>
2019-08-26 17:46:11 +0000

    This is 14th commit: Lesson 16

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,9 +3,13 @@ class QuotesController < ApplicationController
     @quote = Quote.order("RANDOM()").first
   end
   
+=begin  
   def new
     @quote = Quote.new
-  end
+  end  
+ # --> Since we moved our form into our modal, we don't need the actual page where the form was running before anymore.
+ # --> We can also delete the actual file that contained our form at app/views/quotes/new.html.erb
+=end 
   
   def create
     @quote = Quote.create(quote_params)
